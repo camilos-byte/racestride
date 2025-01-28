f8799fa68fddbaf744cb0a679b80b559da44a7be
El_inge <64440659+camilos-byte@users.noreply.github.com>
2025-01-27 20:42:53 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  webpack: (config, { isServer }) => {
+        // Configuración de Webpack para la caché
+        config.cache = {
+            type: 'filesystem',
+            buildDependencies: {
+                config: [__filename], // Archivo actual
+            },
+            version: '1.0', // Cambia según lo que necesites
+        };
+
+        // Retorna la configuración modificada
+        return config;
+    },
 };
 
 module.exports = nextConfig;
